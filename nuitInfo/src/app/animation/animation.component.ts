d17221403c79add0ce2801ee9ab3f36ca6bff489
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-12-08 06:04:34 +0100

    [FIX] Sounds effects working

--- nuitInfo/src/app/animation/animation.component.ts
+++ nuitInfo/src/app/animation/animation.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-animation',
   templateUrl: './animation.component.html',
-  styleUrl: './animation.component.scss'
+  styleUrls: ['./animation.component.scss']
 })
-export class AnimationComponent {
+export class AnimationComponent implements OnInit {
+  @ViewChild('backgroundMusic') backgroundMusic!: ElementRef;
+
   numberOfImages = 1;
+
+  ngOnInit() {
+    this.playBackgroundMusic();
+  }
+
+  playBackgroundMusic() {
+    const audio: HTMLAudioElement = this.backgroundMusic.nativeElement;
+    audio.play().catch((error) => {
+      console.error('Error playing background music:', error);
+    });
+  }
 }
